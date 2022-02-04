1bd82af4b7d7582931b9a522a1a10b5d89050dff
조은학 <02069@naver.com>
2022-02-04 23:32:00 +0900

    python 소켓통신 과제 추가

--- tcp/python/multi_client.py
+++ tcp/python/multi_client.py
@@ -1,7 +1,37 @@
-class MultiClient :
-	def __init__(self) :
-		self.sockfd = 0
-		self.inUse = 0
-		self.tid = tid
-		self.uid = uid
+import socket
+from _thread import *
+# import signal
 
+ADDR = input("Enter Server Address: ")
+PORT = 9999	
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((ADDR, PORT))
+
+def receive_data(client_socket) :
+    while True :
+        data = client_socket.recv(1024)
+
+        print(str(data.decode()))
+
+start_new_thread(receive_data, (client_socket,))
+name = input("Enter Your Name : ")
+client_socket.send(name.encode())
+
+# def sigint_handler(signo, frame) :
+# 	client_socket.close()
+# 	print("Client Closed....\n")
+# 	exit()
+
+# signal.signal(signal.SIGINT, sigint_handler)
+
+while True :
+
+	message = input()
+
+	if message == 'quit': 
+		break
+	
+	client_socket.send(message.encode())
+
+client_socket.close()
